3a15774d8874a153f14ec1bd15bb5c805126058a
Mei Beth <mbshepherd1@buffs.wtamu.edu>
2024-03-26 22:46:35 -0500

    users login logout functionality

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -46,6 +46,7 @@ class BookAdmin(admin.ModelAdmin):
 # Register the Admin classes for BookInstance using the decorator
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'borrower', 'due_back', 'id')
     list_filter = ('status', 'due_back')
 
     fieldsets = (
@@ -53,6 +54,6 @@ class BookInstanceAdmin(admin.ModelAdmin):
             'fields': ('book', 'imprint', 'id')
         }),
         ('Availability', {
-            'fields': ('status', 'due_back')
+            'fields': ('status', 'due_back', 'borrower')
         }),
     )
--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -1,5 +1,7 @@
 import uuid  # Required for unique book instances
 from django.db import models
+from django.conf import settings
+from datetime import date
 
 # Create your models here.
 
@@ -101,6 +103,14 @@ class BookInstance(models.Model):
     book = models.ForeignKey('Book', on_delete=models.RESTRICT, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
+    borrower = models.ForeignKey(settings.AUTH_USER_MODEL,
+                                 on_delete=models.SET_NULL,
+                                 null=True, blank=True)
+
+    @property
+    def is_overdue(self):
+        """Determines if the book is overdue based on due date and current date."""
+        return bool(self.due_back and date.today() > self.due_back)
 
     LOAN_STATUS = (
         ('m', 'Maintenance'),
